0ade5215582a5ec53eea25ffb5a39437d81073ba
Iimamtzh <iimamtzhthriiee21@gmail.com>
2023-08-03 16:34:45 +0700

    Tambah Post type mobil

--- functions.php
+++ functions.php
@@ -1,7 +1,21 @@
 <?php
-
 /* Loads parent stylesheet */
-add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
-function wpchild_enqueue_styles(){
-  wp_enqueue_style( 'sweetweb-style', get_template_directory_uri() . '/style.css' );
+add_action('wp_enqueue_scripts', 'wpchild_enqueue_styles');
+function wpchild_enqueue_styles()
+{
+  wp_enqueue_style('sweetweb-style', get_template_directory_uri() . '/style.css');
+}
+
+
+// Sweetweb's includes directory.
+$sweetweb_inc_dir = 'inc';
+
+// Array of files to include.
+$sweetweb_includes = array(
+  'post-type.php',                  // Initialize theme default settings.
+);
+
+// Include files.
+foreach ($sweetweb_includes as $file) {
+  require_once get_stylesheet_directory() . '/' . $sweetweb_inc_dir . '/' . $file;
 }
---
+++ inc/post-type.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Sweetweb Theme Customizer
+ *
+ * @package Sweetweb Dealer
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+// membuat post type mobil
+function create_post_type_mobil()
+{
+    register_post_type(
+        'mobil',
+        array(
+            'labels' => array(
+                'name' => 'Mobil',
+                'singular_name' => 'Mobil'
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'mobil'),
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon'   => 'dashicons-car',
+            'show_in_rest' => true
+        )
+    );
+}
+add_action('init', 'create_post_type_mobil');
